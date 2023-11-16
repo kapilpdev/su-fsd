ad8522670f13f3da7cadde350faf891d867f4b20
kapildev <kapilppatel03@gmail.com>
2023-11-16 20:26:34 +0530

    modify the sorting logic

--- src/pages/api/sortedByFileName.ts
+++ src/pages/api/sortedByFileName.ts
@@ -10,6 +10,12 @@ type Data = {
   }[];
 };
 
+// Function to extract numerical values from a string
+const extractNumericValue = (str: string): number | string => {
+  const num = parseInt(str, 10);
+  return isNaN(num) ? str : num;
+};
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -21,6 +27,17 @@ export default function handler(
     const jsonData = csvData.split('\n').map((line) => {
       const [createdAt, fileName] = line.split(';');
       return { createdAt, fileName };
+    });
+
+    // Sorting by fileName, considering numeric values numerically
+    jsonData.sort((a, b) => {
+      const fileNameA = a.fileName.replace(/^0+/, ''); // Remove leading zeros
+      const fileNameB = b.fileName.replace(/^0+/, ''); // Remove leading zeros
+
+      const valueA = extractNumericValue(fileNameA);
+      const valueB = extractNumericValue(fileNameB);
+
+      return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
     });
 
     res.status(200).json({ data: jsonData });
